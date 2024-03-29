fef927cf5fbd2951ca86020a84b26d858eee040f
tkarube <karubetomokazu@gmail.com>
2024-03-28 18:49:33 -0700

    mv vimrc init.vim 2

--- createMyShellEnv.sh
+++ createMyShellEnv.sh
@@ -20,7 +20,7 @@ mkdir -p ~/.config/nvim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl https://raw.githubusercontent.com/tkarube/my-dotfiles/main/init.vim -o ~/.config/nvim
+curl https://raw.githubusercontent.com/tkarube/my-dotfiles/main/init.vim -o ~/.config/nvim/init.vim
 vim +'PlugInstall --sync' +qa
 
 # install zshrc
